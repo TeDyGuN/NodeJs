47fe7651d9449d64d32f387e5657a53c5ae36468
tedygun <ted_cc@hotmail.com>
2017-03-24 00:19:39 -0400

    Leer Archivos y Programacion Asincrona

--- hello.js
+++ hello.js
@@ -1,11 +1,9 @@
 var http = require('http'),
     fs   = require('fs');
 
-var html = fs.readFile('./index.html', function(err, data){
-  
+fs.readFile('./index.html', function(err, data){
+  http.createServer(function(req, res){
+      res.write(data);
+      res.end();
+  }).listen(3000);
 });
-
-http.createServer(function(req, res){
-    res.write(html);
-    res.end();
-}).listen(3000);

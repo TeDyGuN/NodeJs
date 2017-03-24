747c8e9218b5b4d523686982d2db7fb48648e72a
tedygun <ted_cc@hotmail.com>
2017-03-24 01:02:40 -0400

    Variables en una Vista

--- hello.js
+++ hello.js
@@ -4,8 +4,21 @@ var http = require('http'),
 
 http.createServer(function(req, res){
   fs.readFile('./index.html', function(err, data){
-    res.writeHead(200, {"Content-Type":"application/json"});
-    res.write(JSON.stringify({nombre: "Ted Carrasco", username:"tedygun"}));
+    var html_string = data.toString();
+    //Expresion regular que busca {x}
+    var variables = html_string.match(/[^\{\}]+(?=\})/g);
+    var nombre = 'Ted';
+
+    for (var i = 0; i < variables.length; i++) {
+      //lo ejecutamos con codigo de javascript
+      var value = eval(variables[i]);
+      //Reemplazar el contenido de llaves por su valor correspondiente
+      html_string = html_string.replace("{"+variables[i]+"}", value);
+    }
+
+
+    res.writeHead(200, {"Content-Type":"text/html"});
+    res.write(html_string);
     res.end();
   });
 }).listen(3000);

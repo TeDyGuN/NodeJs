3fefccf4812da8dde701cbacee550caf4a1232e0
tedygun <ted_cc@hotmail.com>
2017-03-24 00:30:55 -0400

    Encabezados y el objeto response

--- hello.js
+++ hello.js
@@ -1,9 +1,11 @@
 var http = require('http'),
     fs   = require('fs');
 
-fs.readFile('./index.html', function(err, data){
-  http.createServer(function(req, res){
-      res.write(data);
-      res.end();
-  }).listen(3000);
-});
+
+http.createServer(function(req, res){
+  fs.readFile('./index.html', function(err, data){
+    res.writeHead(200, {"Content-Type":"application/json"});
+    res.write(JSON.stringify({nombre: "Ted Carrasco", username:"tedygun"}));
+    res.end();
+  });
+}).listen(3000);
